7b854604dddb1583832ecb944dc7a704efc5820a
Michael Voitovich <zx.hunter@gmail.com>
2025-08-08 04:49:16 +0300

    Add animated generator icon system with manual activation support

--- main/board_factorys.go
+++ main/board_factorys.go
@@ -7,6 +7,10 @@ components {
   component: "/main/token_factory.factory"
 }
 components {
+  id: "generator_icon_factory"
+  component: "/main/generator_icon_factory.factory"
+}
+components {
   id: "board"
   component: "/scripts/board.script"
 }
---
+++ main/generator_icon.go
@@ -0,0 +1,25 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"energy_icon\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 32.0\n"
+  "  y: 32.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/tile.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 20.0
+    y: -20.0
+    z: 0.1
+  }
+  scale {
+    x: 0.5
+    y: 0.5
+    z: 1.0
+  }
+}
